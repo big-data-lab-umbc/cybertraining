d6b7d81914873d2f10173a90ab89787dc56c7a41
Pei427 <guopei90427@gmail.com>
2018-05-08 16:59:26 -0400

    1

--- 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
+++ 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
@@ -47,18 +47,18 @@ if __name__ == "__main__":
 
     sparkDf.first()
 
-    data = [(Vectors.sparse(4, [(0, 1.0), (3, -2.0)]),),
-            (Vectors.dense([4.0, 5.0, 0.0, 3.0]),),
-            (Vectors.dense([6.0, 7.0, 0.0, 8.0]),),
-            (Vectors.sparse(4, [(0, 9.0), (3, 1.0)]),)]
-
-    df = spark.createDataFrame(data, ["features"])
-
-    r1 = Correlation.corr(df, "features").head()
-    print("Pearson correlation matrix:\n" + str(r1[0]))
-
-    r2 = Correlation.corr(df, "features", "spearman").head()
-    print("Spearman correlation matrix:\n" + str(r2[0]))
+    # data = [(Vectors.sparse(4, [(0, 1.0), (3, -2.0)]),),
+    #         (Vectors.dense([4.0, 5.0, 0.0, 3.0]),),
+    #         (Vectors.dense([6.0, 7.0, 0.0, 8.0]),),
+    #         (Vectors.sparse(4, [(0, 9.0), (3, 1.0)]),)]
+    #
+    # df = spark.createDataFrame(data, ["features"])
+    #
+    # r1 = Correlation.corr(df, "features").head()
+    # print("Pearson correlation matrix:\n" + str(r1[0]))
+    #
+    # r2 = Correlation.corr(df, "features", "spearman").head()
+    # print("Spearman correlation matrix:\n" + str(r2[0]))
 
     # rdd1 = spark.parallelize(chist)
     # rdd2 = rdd1.map(lambda x: [int(i) for i in x])
