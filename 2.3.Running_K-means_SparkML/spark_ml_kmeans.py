fa0fdff7ecc32a2f6d83b3a1fc6605b66203d37c
Pei427 <guopei90427@gmail.com>
2018-05-08 15:00:07 -0400

    import df

--- 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
+++ 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
@@ -1,6 +1,11 @@
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.stat import Correlation
 from pyspark.sql import SparkSession
+import numpy as np
+import sys
+import os.path
+
+# import csv
 
 if __name__ == "__main__":
     spark = SparkSession \
@@ -8,14 +13,46 @@ if __name__ == "__main__":
         .appName("CorrelationExample") \
         .getOrCreate()
 
-    data = [(Vectors.sparse(4, [(0, 1.0), (3, -2.0)]),),
-            (Vectors.dense([4.0, 5.0, 0.0, 3.0]),),
-            (Vectors.dense([6.0, 7.0, 0.0, 8.0]),),
-            (Vectors.sparse(4, [(0, 9.0), (3, 1.0)]),)]
-    df = spark.createDataFrame(data, ["features"])
 
-    r1 = Correlation.corr(df, "features").head()
-    print("Pearson correlation matrix:\n" + str(r1[0]))
+    def bin_file_read2mtx(fname, dtp=np.float32):
+        """ Open a binary file, and read data
+            fname : file name
+            dtp   : data type; np.float32 or np.float64, etc. """
 
-    r2 = Correlation.corr(df, "features", "spearman").head()
-    print("Spearman correlation matrix:\n" + str(r2[0]))
+        if not os.path.isfile(fname):
+            print("File does not exist:" + fname)
+            sys.exit()
+
+        fd = open(fname, 'rb')
+        bin_mat = np.fromfile(file=fd, dtype=dtp)
+        fd.close()
+        return bin_mat
+
+    indir = '/umbc/xfs1/cybertrn/cybertraining2018/team2/research/kmeans/'
+    infile = indir + 'aquad3c6tvppcl.noMissing.20050101200512313445612x42.float32.dat'
+
+    nelem = 42
+    chist = bin_file_read2mtx(infile)
+    ### In case the size of input data is too large
+    # chist = np.memmap(infile,dtype=np.float32,mode='r')
+
+    n = chist.shape[0]
+    m = n / nelem
+    chist = chist.reshape([m, nelem])
+
+    print(chist.shape)
+    print(chist[000, :])
+
+    # data = [(Vectors.sparse(4, [(0, 1.0), (3, -2.0)]),),
+    #         (Vectors.dense([4.0, 5.0, 0.0, 3.0]),),
+    #         (Vectors.dense([6.0, 7.0, 0.0, 8.0]),),
+    #         (Vectors.sparse(4, [(0, 9.0), (3, 1.0)]),)]
+    df = spark.createDataFrame(chist)
+
+    # r1 = Correlation.corr(df, "features").head()
+    # print("Pearson correlation matrix:\n" + str(r1[0]))
+    #
+    # r2 = Correlation.corr(df, "features", "spearman").head()
+    # print("Spearman correlation matrix:\n" + str(r2[0]))
+
+    df.printSchema()
