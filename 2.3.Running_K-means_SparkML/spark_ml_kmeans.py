83de78a59eeb15b549e102a32f6c9fd94bc06902
Pei427 <guopei90427@gmail.com>
2018-05-08 13:54:34 -0400

    MayaMLtest

---
+++ 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
@@ -0,0 +1,20 @@
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.stat import Correlation
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .appName("CorrelationExample") \
+        .getOrCreate()
+
+    data = [(Vectors.sparse(4, [(0, 1.0), (3, -2.0)]),),
+            (Vectors.dense([4.0, 5.0, 0.0, 3.0]),),
+            (Vectors.dense([6.0, 7.0, 0.0, 8.0]),),
+            (Vectors.sparse(4, [(0, 9.0), (3, 1.0)]),)]
+    df = spark.createDataFrame(data, ["features"])
+
+    r1 = Correlation.corr(df, "features").head()
+    print("Pearson correlation matrix:\n" + str(r1[0]))
+
+    r2 = Correlation.corr(df, "features", "spearman").head()
+    print("Spearman correlation matrix:\n" + str(r2[0]))
