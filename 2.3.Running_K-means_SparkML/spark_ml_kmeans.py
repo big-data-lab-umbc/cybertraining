f042d53d247aade2166c6d620aff6b30e8a00114
Pei427 <guopei90427@gmail.com>
2018-05-08 16:13:54 -0400

    update

--- 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
+++ 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
@@ -43,16 +43,25 @@ if __name__ == "__main__":
     print(chist.shape)
     print(chist[000, :])
 
-    data = [(Vectors.sparse(4, [(0, 1.0), (3, -2.0)]),),
-            (Vectors.dense([4.0, 5.0, 0.0, 3.0]),),
-            (Vectors.dense([6.0, 7.0, 0.0, 8.0]),),
-            (Vectors.sparse(4, [(0, 9.0), (3, 1.0)]),)]
-    df = spark.createDataFrame(chist.tolist())
+    # data = [(Vectors.sparse(4, [(0, 1.0), (3, -2.0)]),),
+    #         (Vectors.dense([4.0, 5.0, 0.0, 3.0]),),
+    #         (Vectors.dense([6.0, 7.0, 0.0, 8.0]),),
+    #         (Vectors.sparse(4, [(0, 9.0), (3, 1.0)]),)]
 
-    r1 = Correlation.corr(df, "features").head()
-    print("Pearson correlation matrix:\n" + str(r1[0]))
+    rdd1 = spark.parallelize(chist)
+    rdd2 = rdd1.map(lambda x: [int(i) for i in x])
 
-    r2 = Correlation.corr(df, "features", "spearman").head()
-    print("Spearman correlation matrix:\n" + str(r2[0]))
+    df = rdd2.toDF(["X1", "X2", "X3", "X4", "X5", "X6", "X7",
+                    "X8", "X9", "X10", "X11", "X12", "X13", "X14",
+                    "X15", "X16", "X17", "X18", "X19", "X20", "X21",
+                    "X22", "X23", "X24", "X25", "X26", "X27", "X28",
+                    "X29", "X30", "X31", "X32", "X33", "X34", "X35",
+                    "X36", "X37", "X38", "X39", "X40", "X41", "X42"])
+
+    # r1 = Correlation.corr(df, "features").head()
+    # print("Pearson correlation matrix:\n" + str(r1[0]))
+    #
+    # r2 = Correlation.corr(df, "features", "spearman").head()
+    # print("Spearman correlation matrix:\n" + str(r2[0]))
     df.first()
     df.printSchema()
