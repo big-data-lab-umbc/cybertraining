cfe2656a383a914c5848e9f87269524beedcdc54
Pei427 <guopei90427@gmail.com>
2018-05-10 21:33:51 -0400

    1

--- 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
+++ 2.3.Running_K-means_SparkML/spark_ml_kmeans.py
@@ -84,11 +84,10 @@ if __name__ == "__main__":
         # print(center[41])
         # for index in range(42):
         #     print(center[index])
-        for i in range(7):
-            for j in range(8):
+        for i in range(6):
+            for j in range(7):
                 for index in range(42):
                     k[i][j] = center[index]
-
 
     print("our k test is like this")
     print("===================")
