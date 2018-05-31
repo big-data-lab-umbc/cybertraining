25b617c72a6166f008bf04b0036f01b362202149
Carlos Barajas <barajasc@umbc.edu>
2018-05-31 03:28:52 -0400

    removing old commented code

--- 2.6.MPI_Cython/k_means_class_py3.py
+++ 2.6.MPI_Cython/k_means_class_py3.py
@@ -1,12 +1,11 @@
 import os.path
 import sys
 import numpy as np
-# from k_means_mod import kmeans_omp_module as km_mod
 import k_means_cython as km_mod
-# from mpi4py import MPI
 import datetime
 from mpi4py import MPI
-class K_means(object):
+
+class K_means:
     """ 
     K-means clustering importing fortran module
 
@@ -65,7 +64,6 @@ class K_means(object):
         """
 
         indata=self._initialize_indata(indata)
-        # km_mod.set_num_threads(num_threads)
         return indata
 
     def _initialize_indata(self,indata):
@@ -77,7 +75,6 @@ class K_means(object):
 
         self.nrec=indata.shape[0]/self.nelem
         data = np.reshape(indata,newshape=[int(self.nrec),int(self.nelem)],order='C').astype(float)
-        # data = np.reshape(indata,newshape=[int(self.nrec),int(self.nelem)],order='C').T.astype(float)
         self.print(data.shape)
         self.startRec, self.stopRec = km_mod.get_record_spans(self.nrec, self.rank, self.tprocs)
         self.print("[{:03d}] {}::{}".format(self.rank,self.startRec,self.stopRec))
@@ -110,7 +107,7 @@ class K_means(object):
 
         idx=int(ncount_per_day*(ntodd+(self.id_+13.)/(self.id_+37.)))
         ctd=[]
-        # ctd.append(indata[:,idx])
+
         ctd.append(indata[idx,:])
         while len(ctd)<self.knum:
             idx+=idx
@@ -118,12 +115,10 @@ class K_means(object):
                 idx-=self.nrec
                 self.print("idx is over total record")
 
-            # tmpctd=indata[:,idx]
-            tmpctd=indata[idx,:]
+            tmpctd=indata[int(idx),:]
             if self._test_dist(ctd,tmpctd,ini_ctd_dist_min):
                 ctd.append(tmpctd)
 
-        # return np.asfarray(ctd).T
         return np.asfarray(ctd)
 
     def _test_dist(self,group1,elem1,criterion):
@@ -149,7 +144,6 @@ class K_means(object):
         self.print("***** nk= {}".format(nk))
         totalTime = 0
         for it in range(iter_max):
-        # for it in range(1):
             self.print("***** {}".format(it+1))
             startTime = MPI.Wtime()
             ### Assign data to centroid and get new sum (not mean yet)
@@ -157,32 +151,34 @@ class K_means(object):
             maxmove=0.
             cl_count= np.zeros(self.knum)
             for ic in range(self.knum):
-                # idx= cl==ic+1
                 # Pure Python should use this and initialize with ncl
                 idx= cl==ic
-                # cl_count.append(idx.sum())
-                # MPI Reduce on cl_count
+
+                # MPI Reduce for the partial sum of cl_count
                 val = idx.sum()
                 tmp_count = self.comm.allreduce(val, op=MPI.SUM)
                 cl_count[ic] = tmp_count
-                # tmpctd=outsum[:,ic]/float(cl_count[-1])
-                # move=km_mod.calc_dist(tmpctd,ctd[:,ic])
+                
+                # Merge the partial sums
                 fodder = outsum[ic,:]/float(cl_count[ic])
                 tmpctd = np.empty(shape=fodder.shape,order='C')
+
                 # MPI Reduce on tmpctd -> since cl==ncl doesn't matter, this works
                 self.comm.Allreduce(fodder, tmpctd, op=MPI.SUM)
-                # self.comm.Barrier()
+
                 # Parallel distance calculation? -> Data structure is too small to matter
                 move=km_mod.calc_dist(tmpctd,ctd[ic,:])
 
                 self.print("* {:02d} {}".format(ic+1,move))
 
                 maxmove=max(maxmove,move)
-                # ctd[:,ic]=tmpctd
+    
                 ctd[ic,:]=tmpctd
+            # 4 Performance Tests!
             endTime = MPI.Wtime()
             thisTime = endTime-startTime
             totalTime += thisTime
+
             self.print("** {}".format(datetime.timedelta(seconds=(thisTime))))
             if n10>0 and maxmove < self.epsilon*10.**n10:
                 ### Speeding up trick1
@@ -201,19 +197,17 @@ class K_means(object):
             startTime = MPI.Wtime()
             # Wcv is a partial sum of only one set of records
             wcvsum  = km_mod.get_wcv_sum(indata,ctd,cl,self.startRec,self.stopRec)
-            # self.comm.Allreduce(fodder, wcvsum, op=MPI.SUM)
+
             endTime = MPI.Wtime()
             thisTime = endTime - startTime
-            # wcv=wcvsum.sum(axis=0)/np.asfarray(cl_count)
+            # Merge the partial sum into a smaller sum
             fodder = wcvsum.sum(axis=1)
             wcv = np.empty(shape=fodder.shape)
             self.comm.Allreduce(fodder, wcv, op=MPI.SUM)
             wcv=wcv/cl_count
             
-            # cf=ctd.sum(axis=0)
             cf=ctd.sum(axis=1)
             self.print("** Knum= {}, ID= {}, Total WCV= {}, LowestCF WCV={}, WCV Time= {}".format(
-                # self.knum,self.id_,wcv.sum(),wcv[np.argsort(cf)[0]],datetime.timedelta(seconds=thisTime)))
                 self.knum,self.id_,wcv.sum(),wcv[np.argsort(cf)[0]],datetime.timedelta(seconds=thisTime)))
 
         return ctd
