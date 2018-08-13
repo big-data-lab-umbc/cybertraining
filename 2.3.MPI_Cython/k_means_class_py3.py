00a7da8c1d9136dc1d7001b811502a1f9eb77d7b
Carlos Barajas <barajasc@umbc.edu>
2018-08-13 09:38:21 -0400

    fixed a bug where startRec and stopRec where being used before definition

--- 2.3.MPI_Cython/k_means_class_py3.py
+++ 2.3.MPI_Cython/k_means_class_py3.py
@@ -163,6 +163,9 @@ class K_means:
         self.comm.Bcast([ctd, MPI.DOUBLE],root=0)
         self.Allprint(ctd.shape)
         # Read in only relevant records to memory
+        self.startRec, self.stopRec = km_mod.get_record_spans(self.nrec, self.rank, self.tprocs)
+        self.totalRec = self.stopRec - self.startRec
+        self.print(data.shape)
         bites = np.dtype(dtp).itemsize
         offset = self.startRec * bites
         with open(fname, "rb") as inFile:
