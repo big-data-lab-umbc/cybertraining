bd87c2fe8908fea24d37c173153faf713b79106b
webzerg <huangxin0921@gmail.com>
2020-08-04 22:59:58 -0400

    create wget for noaa

---
+++ year-3-projects/team-3/Data/Simulation/NOAA Data/wget.sh
@@ -0,0 +1,13 @@
+wget http://esgdata.gfdl.noaa.gov/thredds/fileServer/gfdl_dataroot4/CMIP/NOAA-GFDL/GFDL-CM4/amip/r1i1p1f1/CFmon/cllcalipso/gr1/v20180701/cllcalipso_CFmon_GFDL-CM4_amip_r1i1p1f1_gr1_200301-200812.nc
+
+wget: http://esgdata.gfdl.noaa.gov/thredds/fileServer/gfdl_dataroot4/CMIP/NOAA-GFDL/GFDL-CM4/amip/r1i1p1f1/CFmon/clhcalipso/gr1/v20180701/clhcalipso_CFmon_GFDL-CM4_amip_r1i1p1f1_gr1_200301-200812.nc
+
+wget http://esgdata.gfdl.noaa.gov/thredds/fileServer/gfdl_dataroot4/CMIP/NOAA-GFDL/GFDL-CM4/amip/r1i1p1f1/CFmon/clmcalipso/gr1/v20180701/clmcalipso_CFmon_GFDL-CM4_amip_r1i1p1f1_gr1_200301-200812.nc
+
+wget http://esgdata.gfdl.noaa.gov/thredds/fileServer/gfdl_dataroot4/CMIP/NOAA-GFDL/GFDL-CM4/amip/r1i1p1f1/CFmon/cltcalipso/gr1/v20180701/cltcalipso_CFmon_GFDL-CM4_amip_r1i1p1f1_gr1_200301-200812.nc
+
+wget http://esgdata.gfdl.noaa.gov/thredds/fileServer/gfdl_dataroot4/CMIP/NOAA-GFDL/GFDL-CM4/amip/r1i1p1f1/Amon/rsdt/gr1/v20180701/rsdt_Amon_GFDL-CM4_amip_r1i1p1f1_gr1_200301-200812.nc
+
+wget http://esgdata.gfdl.noaa.gov/thredds/fileServer/gfdl_dataroot4/CMIP/NOAA-GFDL/GFDL-CM4/amip/r1i1p1f1/Amon/rsutcs/gr1/v20180701/rsutcs_Amon_GFDL-CM4_amip_r1i1p1f1_gr1_200301-200812.nc
+
+wget http://esgdata.gfdl.noaa.gov/thredds/fileServer/gfdl_dataroot4/CMIP/NOAA-GFDL/GFDL-CM4/amip/r1i1p1f1/Amon/rsut/gr1/v20180701/rsut_Amon_GFDL-CM4_amip_r1i1p1f1_gr1_200301-200812.nc
